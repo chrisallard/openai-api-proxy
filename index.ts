d63106f1e5ce606c5ef95c06f22953eeedcbe98d
Chris Allard <christopher.david.allard@gmail.com>
2024-05-02 14:30:16 -0600

    openai proxy integration

--- index.ts
+++ index.ts
@@ -4,12 +4,23 @@ import {
   APIGatewayProxyResultV2,
   Handler,
 } from "aws-lambda";
+import OpenAI from "openai";
 
-const client = new SSMClient();
-
+let key = "";
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResultV2> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(getWord()),
+    headers: {
+      Context: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+};
+
+const getWord = async () => {
   const client = new SSMClient();
   const input = {
     Name: "/wordal/openai-api-key",
@@ -18,8 +29,22 @@ export const handler: Handler = async (
 
   const command = new GetParameterCommand(input);
   const response = await client.send(command);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-  };
+
+  const openai = new OpenAI({
+    apiKey: response.Parameter.Value,
+  });
+
+  const completion = await openai.chat.completions.create({
+    response_format: { type: "json_object" },
+    messages: [
+      {
+        role: "system",
+        content:
+          "Give me a common 5 letter word in English and 2 words, as an array, that rhyme with it to output JSON.",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  return completion.choices[0].message.content;
 };
